7892b0eff0bf579705d58c072754d43329b557da
Omer Faruk <154370292+omerfaruk2626@users.noreply.github.com>
2025-05-11 01:13:55 +0300

    changed to lib qrcode

--- src/app/qrcode/page.jsx
+++ src/app/qrcode/page.jsx
@@ -1,18 +1,19 @@
 "use client";
-import { QRCode } from "qrcode.react";
+import QRCode from "react-qr-code";
 
 export default function QRCodePage() {
-  const uploadUrl = "https://melihanim.vercel.app/upload";
+  const uploadUrl = "https://kadikuyusu.com/upload";
 
   return (
-    <div className="min-h-screen bg-gray-100 flex flex-col items-center justify-center p-8">
+    <div className="min-h-screen bg-gray-100 flex flex-col items-center justify-center p-6">
       <h1 className="text-2xl font-bold mb-4 text-center">
-        📸 Fotoğraf Yükleme Sayfası QR Kodu
+        📸 Yükleme Sayfası QR Kodu
       </h1>
-      <QRCode value={uploadUrl} size={256} />
-      <p className="mt-4 text-gray-600 text-center max-w-md">
-        Aşağıdaki QR kodu telefonunuzla tarayarak fotoğraflarınızı kolayca
-        yükleyebilirsiniz.
+      <div className="bg-white p-6 rounded shadow">
+        <QRCode value={uploadUrl} size={256} />
+      </div>
+      <p className="mt-4 text-gray-700 max-w-md text-center">
+        Telefonunuzla QR kodu tarayarak kolayca fotoğraf yükleyebilirsiniz.
       </p>
     </div>
   );
